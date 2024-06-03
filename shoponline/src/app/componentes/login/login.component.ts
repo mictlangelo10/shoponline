2dc374e5f8e1a4f2d3de46e6dbe1cf68dc3eec61
mictlangelo10 <123588416+mictlangelo10@users.noreply.github.com>
2024-06-03 11:41:50 -0600

    Creando Interfaz de Login

--- shoponline/src/app/componentes/login/login.component.ts
+++ shoponline/src/app/componentes/login/login.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/
+        ),
+      ],
+    ],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  get email() {
+    return this.loginForm.controls['email'];
+  }
+
+  get password() {
+    return this.loginForm.controls['password'];
+  }
 }
--- shoponline/src/app/componentes/navbar/navbar.component.ts
+++ shoponline/src/app/componentes/navbar/navbar.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+import { Router } from '@angular/router'; // Importa Router aquí
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css',
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
   items: MenuItem[] | any;
+
+  constructor(private router: Router) {} // Inyecta Router en el constructor
 
   ngOnInit() {
     this.items = [
@@ -16,10 +19,16 @@ export class NavbarComponent {
           {
             label: 'Ingresar',
             icon: 'pi pi-sign-in',
+            command: () => {
+              this.router.navigate(['/login']); // Agrega esta línea para redirigir al login
+            },
           },
           {
             label: 'Registrar',
             icon: 'pi pi-user-plus',
+            command: () => {
+              this.router.navigate(['/register']); // Opcional para redirigir al registro
+            },
           },
         ],
       },
